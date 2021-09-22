36a287340f39ea802659f2f49d2077d3f3afcdfd
Juan Carlos Mendoza R <jcarlosmr@gmail.com>
2021-09-22 00:35:41 -0500

    Change certs path and bits to 4096

--- prepare-server.sh
+++ prepare-server.sh
@@ -5,11 +5,11 @@ set -eu
 #
 # Prepare the certificate authority (self-signed).
 #
-cd /rbmq/testca
+cd /rbmq/ca
 
 # Create a self-signed certificate that will serve a certificate authority (CA).
 # The private key is located under "private".
-openssl req -x509 -config openssl.cnf -newkey rsa:2048 -days 365 -out cacert.pem -outform PEM -subj /CN=MyTestCA/ -nodes
+openssl req -x509 -config openssl.cnf -newkey rsa:4096 -days 365 -out cacert.pem -outform PEM -subj /CN=MyTestCA/ -nodes
 
 # Encode our certificate with DER.
 openssl x509 -in cacert.pem -out cacert.cer -outform DER
@@ -22,14 +22,13 @@ openssl x509 -in cacert.pem -out cacert.cer -outform DER
 cd /rbmq/server
 
 # Generate a private RSA key.
-openssl genrsa -out key.pem 2048
+openssl genrsa -out key.pem 4096
 
 # Generate a certificate from our private key.
-#openssl req -new -key key.pem -out req.pem -outform PEM -subj /CN=$(hostname)/O=server/ -nodes
 openssl req -new -key key.pem -out req.pem -outform PEM -subj /CN=$DOMAIN/O=server/ -nodes
 
 # Sign the certificate with our CA.
-cd /rbmq/testca
+cd /rbmq/ca
 openssl ca -config openssl.cnf -in /rbmq/server/req.pem -out /rbmq/server/cert.pem -notext -batch -extensions server_ca_extensions
 
 # Create a key store that will contain our certificate.
